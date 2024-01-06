3604fab9c3baf2a2284e02851bd6f9bcb3147f6a
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2024-01-07 00:54:26 +0700

    membuat data class Pesanan

--- app/src/main/java/com/example/finaluas/data/datapesanan.kt
+++ app/src/main/java/com/example/finaluas/data/datapesanan.kt
@@ -3,11 +3,12 @@ package com.example.finaluas.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-
-@Entity(tableName = "tbl_admin")
-data class Admin(
+@Entity(tableName = "tbl_pesanan")
+data class Pesanan(
     @PrimaryKey(autoGenerate = true)
     val id : Int = 0,
-    val username : String,
-    val pass : String
+    val nama : String,
+    val nohp : String,
+    val alamat : String,
+    val jaminan : String
 )
