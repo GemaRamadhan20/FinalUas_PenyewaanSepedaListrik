6adeaef070a6103ebfb4bd5b5f8d5423acf28e8b
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2024-01-07 00:37:55 +0700

    membuat dataclass admin

---
+++ app/src/main/java/com/example/finaluas/data/admin.kt
@@ -0,0 +1,13 @@
+package com.example.finaluas.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "tbl_admin")
+data class Admin(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val username : String,
+    val password : String
+)
