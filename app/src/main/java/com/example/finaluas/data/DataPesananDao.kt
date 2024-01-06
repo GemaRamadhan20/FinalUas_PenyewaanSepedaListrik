d9c7b569424efe7a1fb117d95a25e8511b4c6966
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2024-01-07 01:06:07 +0700

    membuat interface DataPesananDao

---
+++ app/src/main/java/com/example/finaluas/data/DataPesananDao.kt
@@ -0,0 +1,27 @@
+package com.example.finaluas.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DataPesananDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(pesanan: Pesanan)
+
+    @Update
+    suspend fun update(pesanan: Pesanan)
+
+    @Delete
+    suspend fun delete(pesanan: Pesanan)
+
+    @Query("SELECT * from tbl_pesanan WHERE id = id")
+    fun getPesanan(id: Int): Flow<Pesanan>
+
+    @Query("SELECT * from tbl_pesanan ORDER BY nama ASC")
+    fun getAllPesanan(): Flow<List<Pesanan>>
+}
