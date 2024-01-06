11da78a6fba7028e4a13936eed0046ec7cda520b
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2024-01-07 02:05:51 +0700

    membuat ContainerApp

---
+++ app/src/main/java/com/example/finaluas/repository/ContainerApp.kt
@@ -0,0 +1,14 @@
+package com.example.finaluas.repository
+
+import android.content.Context
+import com.example.finaluas.data.DatabasePesanan
+
+interface ContainerApp {
+    val repositoryPesanan : RepositoryPesanan
+}
+
+class ContainerDataApp(private val context: Context): ContainerApp{
+    override val repositoryPesanan: RepositoryPesanan by lazy {
+        OfflineRepositoryPesanan(DatabasePesanan.getDatabase(context).DataPesananDao())
+    }
+}
