e96f7684585cac989bf0ca2e2609f0c02f223b51
TA1059 <muchsen.ft21@mail.umy.ac.id>
2024-01-09 01:40:29 +0700

    membuat fun PesananBody

--- app/src/main/java/com/example/finaluas/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/finaluas/ui/halaman/HalamanEntry.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,7 +15,35 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import com.example.finaluas.model.DetailPesanan
+import com.example.finaluas.model.UIStatePesanan
 import com.example.kontak.R
+
+@Composable
+fun EntryPesananBody(
+    uiStatePesanan: UIStatePesanan,
+    onPesananValueChange: (DetailPesanan) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        FormInputPesanan(
+            detailPesanan = uiStatePesanan.detailPesanan,
+            onValueChange = onPesananValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            enabled = uiStatePesanan.isEntryValid,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(id = R.string.btn_submit))
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
