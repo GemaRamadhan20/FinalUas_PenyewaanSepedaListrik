2cd4b81eaae6d53ee4d67410eb31d1d711359c9e
TA1059 <muchsen.ft21@mail.umy.ac.id>
2024-01-09 00:07:15 +0700

    membuat private fun ItemDetailRow

--- app/src/main/java/com/example/finaluas/ui/halaman/HalamanDetail.kt
+++ app/src/main/java/com/example/finaluas/ui/halaman/HalamanDetail.kt
@@ -1,13 +1,28 @@
 package com.example.finaluas.ui.halaman
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import com.example.kontak.R
 
+
+@Composable
+private fun ItemDetailRow(
+    @StringRes labelResID: Int, itemDetail: String, modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Text(text = stringResource(labelResID))
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
+    }
+}
 @Composable
 private fun DeleteConfirmDialog(
     onDeleteConfirm: () -> Unit,
