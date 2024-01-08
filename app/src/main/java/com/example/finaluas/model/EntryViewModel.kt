9316da1787b5c4bf2be72d917f09295e5491f08e
TA1059 <muchsen.ft21@mail.umy.ac.id>
2024-01-09 01:34:45 +0700

    membuat fun FormInputPesanan

--- app/src/main/java/com/example/finaluas/model/EntryViewModel.kt
+++ app/src/main/java/com/example/finaluas/model/EntryViewModel.kt
@@ -21,35 +21,36 @@ class EntryViewModel(private val repositoryPesanan: RepositoryPesanan): ViewMode
     }
 
 
-    data class UIStatePesanan(
+
+}
+data class UIStatePesanan(
     val detailPesanan: DetailPesanan = DetailPesanan(),
     val isEntryValid: Boolean = false
-    )
+)
 
 
-    data class DetailPesanan(
-        val id : Int = 0,
-        val nama : String = "",
-        val nohp : String = "",
-        val alamat : String="",
-        val jaminan : String=""
-    )
-    fun DetailPesanan.toPesanan(): Pesanan = Pesanan(
-        id = id,
-        nama = nama,
-        nohp = nohp,
-        alamat = alamat,
-        jaminan = jaminan
-    )
-    fun Pesanan.toUiStatePesanan(isEntryValid: Boolean = false): UIStatePesanan = UIStatePesanan(
-        detailPesanan = this.toDetailPesanan(),
-        isEntryValid = isEntryValid
-    )
-    fun Pesanan.toDetailPesanan(): DetailPesanan = DetailPesanan(
-        id = id,
-        nama = nama,
-        nohp = nohp,
-        alamat = alamat,
-        jaminan = jaminan
-    )
-}
+data class DetailPesanan(
+    val id : Int = 0,
+    val nama : String = "",
+    val nohp : String = "",
+    val alamat : String="",
+    val jaminan : String=""
+)
+fun DetailPesanan.toPesanan(): Pesanan = Pesanan(
+    id = id,
+    nama = nama,
+    nohp = nohp,
+    alamat = alamat,
+    jaminan = jaminan
+)
+fun Pesanan.toUiStatePesanan(isEntryValid: Boolean = false): UIStatePesanan = UIStatePesanan(
+    detailPesanan = this.toDetailPesanan(),
+    isEntryValid = isEntryValid
+)
+fun Pesanan.toDetailPesanan(): DetailPesanan = DetailPesanan(
+    id = id,
+    nama = nama,
+    nohp = nohp,
+    alamat = alamat,
+    jaminan = jaminan
+)
---
+++ app/src/main/java/com/example/finaluas/ui/halaman/HalamanEntry.kt
@@ -0,0 +1,78 @@
+package com.example.finaluas.ui.halaman
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import com.example.finaluas.model.DetailPesanan
+import com.example.kontak.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputPesanan(
+    detailPesanan: DetailPesanan,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailPesanan) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        OutlinedTextField(
+            value = detailPesanan.nama,
+            onValueChange = { onValueChange(detailPesanan.copy(nama = it)) },
+            label = { Text(stringResource(R.string.username)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailPesanan.nohp,
+            onValueChange = { onValueChange(detailPesanan.copy(nohp = it)) },
+            label = { Text(stringResource(R.string.nohp)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailPesanan.alamat,
+            onValueChange = { onValueChange(detailPesanan.copy(alamat = it)) },
+            label = { Text(stringResource(R.string.alamat)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailPesanan.jaminan,
+            onValueChange = { onValueChange(detailPesanan.copy(jaminan = it)) },
+            label = { Text(stringResource(R.string.jaminan)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        if (enabled) {
+            Text(
+                text = stringResource(id = R.string.required_field),
+                modifier = Modifier.padding(start = dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+        Divider(
+            thickness = dimensionResource(id = R.dimen.padding_small),
+            modifier = Modifier.padding(
+                bottom = dimensionResource(
+                    id = R.dimen.padding_medium
+                )
+            )
+        )
+    }
+}
