60e9a2934bf6373f6114744de5ed4553833709ea
TA1059 <muchsen.ft21@mail.umy.ac.id>
2024-01-09 00:25:45 +0700

    membuat fun ItemDetails

--- app/src/main/java/com/example/finaluas/ui/halaman/HalamanDetail.kt
+++ app/src/main/java/com/example/finaluas/ui/halaman/HalamanDetail.kt
@@ -1,16 +1,86 @@
 package com.example.finaluas.ui.halaman
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import com.example.finaluas.data.Pesanan
 import com.example.kontak.R
+
+@Composable
+fun ItemDetails(
+    pesanan: Pesanan, modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier, colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+        ) {
+            ItemDetailRow(
+                labelResID = R.string.username,
+                itemDetail = pesanan.nama,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(
+                        id = R.dimen
+                            .padding_medium
+                    )
+                )
+            )
+            ItemDetailRow(
+                labelResID = R.string.nohp,
+                itemDetail = pesanan.nohp,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(
+                        id = R.dimen
+                            .padding_medium
+                    )
+                )
+            )
+            ItemDetailRow(
+                labelResID = R.string.alamat,
+                itemDetail = pesanan.alamat,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(
+                        id = R.dimen
+                            .padding_medium
+                    )
+                )
+            )
+            ItemDetailRow(
+                labelResID = R.string.jaminan,
+                itemDetail = pesanan.jaminan,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(
+                        id = R.dimen
+                            .padding_medium
+                    )
+                )
+            )
+        }
+
+    }
+}
 
 
 @Composable
